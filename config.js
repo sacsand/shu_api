b475e5a8eae9da548a65c6b2a6fa7630aa1fff5b
sacsand <c4c5nf3@gmail.com>
2016-07-18 19:08:55 +0530

    dsds

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
     'secret': 'shu2k12839',
-    'database': 'mongo ds023425.mlab.com:23425/heroku_0gn01fzn -u <c4c5nf3@gmail.com> -p <foncl1234>'
+    'database': 'mongo ds023425.mlab.com:23425/heroku_0gn01fzn -u c4c5nf3@gmail.com -p foncl1234'
 
 };
