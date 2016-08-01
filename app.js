9ef89c6bbfaae6d8db15ceda6ae2ca30e9c7073f
sacsand <c4c5nf3@gmail.com>
2016-08-01 23:45:35 +0530

    new

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ var Converter   = require('./middleware/converter');
 //mongo db connection
 //var mongoURI = 'mongodb://localhost/test';
 var mongoose = require('mongoose');
-mongoose.connect(config.database||'mongodb://sacsand:sac1234@ds023495.mlab.com:23495/heroku_tdf52rkr' , function(err) {
+//mongoose.connect(config.database||'mongodb://sacsand:sac1234@ds023495.mlab.com:23495/heroku_tdf52rkr' , function(err) {
+mongoose.connect('mongodb://sacsand:sac1234@ds023495.mlab.com:23495/heroku_tdf52rkr' , function(err) {
     if(err) {
         console.log('connection error', err);
     } else {
