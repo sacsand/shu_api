3edbcc325afa64401d6d4023da8112b640eeb6f1
sacsand <c4c5nf3@gmail.com>
2016-08-08 04:35:20 +0530

    n

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ For the Globaly Run use Heroku server. //APK FILE using this server .
 module.exports = {
 
     'secret': 'shu2k12839',
-    'database': 'mongodb://localhost:27017/police'                                             /*local */
-    //'database': 'mongodb://sacsand:sac1234@ds023495.mlab.com:23495/heroku_tdf52rkr'         /*Global Run--heroku server,production */  
+    //'database': 'mongodb://localhost:27017/police'                                             /*local */
+    'database': 'mongodb://sacsand:sac1234@ds023495.mlab.com:23495/heroku_tdf52rkr'         /*Global Run--heroku server,production */
 
 };
